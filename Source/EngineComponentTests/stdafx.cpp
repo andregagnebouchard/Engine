b750e1c06870fb2e12ee1c9e9405f8cbbca4a27e
André Gagné Bouchard <andregagnebouchard@gmail.com>
2017-07-29 14:14:34 -0400

    Added log4cplus as a submodule

--- Source/EngineComponentTests/stdafx.cpp
+++ Source/EngineComponentTests/stdafx.cpp
@@ -1,5 +1,5 @@
 // stdafx.cpp : source file that includes just the standard includes
-// EngineTest.pch will be the pre-compiled header
+// EngineComponentTests.pch will be the pre-compiled header
 // stdafx.obj will contain the pre-compiled type information
 
 #include "stdafx.h"
