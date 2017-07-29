464753214c6e50ed15cfc1acfe9ee6fb2ceb36e7
André Gagné Bouchard <andregagnebouchard@gmail.com>
2017-07-29 10:40:53 -0400

    Added Tinyxml as a submodule

--- EngineTest/EngineTest.cpp
+++ EngineTest/EngineTest.cpp
@@ -2,10 +2,21 @@
 //
 
 #include "stdafx.h"
-
-
+#include <tinyxml\tinyxml2.h>
+using namespace tinyxml2;
 int main()
 {
-    return 0;
+	tinyxml2::XMLDocument xml_doc;
+
+	tinyxml2::XMLError eResult = xml_doc.LoadFile("test.xml");
+	if (eResult != tinyxml2::XML_SUCCESS) return false;
+
+	tinyxml2::XMLNode* root = xml_doc.FirstChildElement("empleados");
+	if (root == nullptr) return false;
+
+	tinyxml2::XMLElement* element = root->FirstChildElement("cantidad");
+	if (element == nullptr) return false;
+
+	return true;
 }
 
