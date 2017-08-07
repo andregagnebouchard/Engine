a15835583a0712fc8a1343e1cfa5997e315e7fb6
André Gagné Bouchard <andregagnebouchard@gmail.com>
2017-08-07 19:09:08 -0400

    Finished importing SFML

---
+++ Source/AeTest/sfml-audio.cpp
@@ -0,0 +1,40 @@
+#include <gtest/gtest.h>
+#include <SFML/Audio.hpp>
+#include <iostream>
+#include <string>
+
+void playSound()
+{
+	// Load a sound buffer from a wav file
+	sf::SoundBuffer buffer;
+	if (!buffer.loadFromFile("../../Support/Testing/sfmlAudioTest//canary.wav"))
+		return;
+
+	// Display sound informations
+	std::cout << "canary.wav:" << std::endl;
+	std::cout << " " << buffer.getDuration().asSeconds() << " seconds" << std::endl;
+	std::cout << " " << buffer.getSampleRate() << " samples / sec" << std::endl;
+	std::cout << " " << buffer.getChannelCount() << " channels" << std::endl;
+
+	// Create a sound instance and play it
+	sf::Sound sound(buffer);
+	sound.play();
+
+	// Loop while the sound is playing
+	while (sound.getStatus() == sf::Sound::Playing)
+	{
+		// Leave some CPU time for other processes
+		sf::sleep(sf::milliseconds(100));
+
+		// Display the playing position
+		std::cout << "\rPlaying... " << sound.getPlayingOffset().asSeconds() << " sec        ";
+		std::cout << std::flush;
+	}
+	std::cout << std::endl << std::endl;
+}
+
+TEST(SFMLAudio, UseSFMLAudio)
+{
+	// Play a sound
+	playSound();
+}
