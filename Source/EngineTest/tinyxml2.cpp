65edf97d08bb1dc68aacc96f8a93d881fe328cd5
André Gagné Bouchard <andre.gagne-bouchard@olympus-ossa.com>
2018-07-03 08:15:03 -0400

    Repaired tinyxml test

--- Source/EngineTest/tinyxml2.cpp
+++ Source/EngineTest/tinyxml2.cpp
@@ -4,15 +4,12 @@ TEST(tinyxml2_test, useTinyXml2)
 {
 	tinyxml2::XMLDocument xml_doc;
 
-	tinyxml2::XMLError eResult = xml_doc.LoadFile("..\\..\\Support\\Testing\\TinyXmlTest.xml");
+	tinyxml2::XMLError eResult = xml_doc.LoadFile("..\\..\\Support\\Testing\\TinyXmlLauncherTest.ini");
 	if (eResult != tinyxml2::XML_SUCCESS) 
 		FAIL();
 
-	tinyxml2::XMLNode* root = xml_doc.FirstChildElement("empleados");
-	if (root == nullptr) 
-		FAIL();
-
-	tinyxml2::XMLElement* element = root->FirstChildElement("cantidad");
-	if (element == nullptr) 
-		FAIL();
+  tinyxml2::XMLNode* launcher = xml_doc.FirstChildElement("DATA")->FirstChildElement("LAUNCHER");
+  std::string processName = launcher->FirstChildElement("PROCESS_NAME")->GetText();
+  size_t screenWidth = atoi(launcher->FirstChildElement("SCREEN_WIDTH")->GetText());
+  size_t screenLength = atoi(launcher->FirstChildElement("SCREEN_LENGTH")->GetText());
 }
